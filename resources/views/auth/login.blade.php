03df8d47188b56fac811dca013e107b352e703fd
pweiss58 <pweiss@hof-university.de>
2018-07-01 13:22:06 +0200

    email added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
+        </div>
+    @endif
+    @if (session('warning'))
+        <div class="alert alert-warning">
+            {{ session('warning') }}
+        </div>
+    @endif
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
