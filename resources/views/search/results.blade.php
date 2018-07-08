896541fecaaad47842ce8e0dde4c4b7fce5cb75d
pweiss58 <pweiss@hof-university.de>
2018-07-08 20:54:50 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -1,12 +1,32 @@
 @extends('layouts.master')
 
 @section('content')
+
+
+    <!-- Search Results -->
     <div>
-        @if(!empty($eventsetresults))
-            @foreach($eventsetresults as $eventsetresult)
-                <h1>{{ $eventsetresult['name'] }} </h1>
-                <br>
-            @endforeach
-        @endif
+
+
+                <div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h4 style="margin-top:40px;margin-bottom:17px;">Suchergebnisse</h4>
+                                @if(!empty($eventsetresults))
+                                    @foreach($eventsetresults as $eventsetresult)
+                                     <div style="background-color:#f1f1f1;padding-top:16px;padding-right:20px;padding-bottom:6px;padding-left:9px;margin-top:16px;margin-left:-6px;">
+                                     <h5 style="color:#313437;">{{ $eventsetresult['name'] }}</h5>
+                                        <p style="color:#313437;">{{ $eventsetresult['shortDescription'] }}</p>
+                                        <a class="d-table-cell justify-content-center learn-more" href="/event/{{$eventsetresult['name']}}" style="background-color:rgba(255,255,255,0);">http://localhost:8000/event/{{$eventsetresult['name']}}
+                                            »</a>
+                                         @endforeach
+                                         @endif
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
     </div>
 @endsection
