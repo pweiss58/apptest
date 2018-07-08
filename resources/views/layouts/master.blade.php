adfa75a625c497f68277f31c01a7123c61f27434
pweiss58 <pweiss@hof-university.de>
2018-07-09 00:29:03 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
                     <div class="input-group"><span class="d-flex justify-content-center align-items-center input-group-addon"
                                 id="basic-addon1" style="height:38px;"><i class="fa fa-search"
                                                                           style="color:rgba(33,37,41,0.5);"></i></span>
-                        <input class="form-control" type="search" placeholder="Suche" aria-describedby="basic-addon1" name="search"></div>
+                        <input class="form-control" type="search" placeholder="Suche" aria-describedby="basic-addon1" name="q"></div>
                 </form>
                 <ul class="nav navbar-nav ml-auto">
                     <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown"
