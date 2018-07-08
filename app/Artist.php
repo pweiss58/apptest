a3aade4cb8048e3e1051bd701f646285e60a3cba
pweiss58 <pweiss@hof-university.de>
2018-07-08 15:25:44 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- app/Artist.php
+++ app/Artist.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace App;
-
+use Laravel\Scout\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class Artist extends Model
 {
+    use Searchable;
     public function event(){
         return $this->belongsToMany('App\Event');
+    }
+
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        return array('name' => $array['name'],);
     }
 }
