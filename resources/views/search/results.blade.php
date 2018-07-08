103977c43c2ee041f2f811f5272278267f04de4a
pweiss58 <pweiss@hof-university.de>
2018-07-09 00:29:24 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -7,26 +7,27 @@
     <div>
 
 
-                <div>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h4 style="margin-top:40px;margin-bottom:17px;">Suchergebnisse ({{$eventsetresults->count()}})</h4>
-                                @if(!empty($eventsetresults))
-                                    @foreach($eventsetresults as $eventsetresult)
-                                     <div style="background-color:#f1f1f1;padding-top:16px;padding-right:20px;padding-bottom:6px;padding-left:9px;margin-top:16px;margin-left:-6px;">
-                                     <h5 style="color:#313437;">{{ $eventsetresult['name'] }}</h5>
-                                        <p style="color:#313437;">{{ $eventsetresult['shortDescription'] }}</p>
-                                        <a class="d-table-cell justify-content-center learn-more" href="/event/{{$eventsetresult['name']}}" style="background-color:rgba(255,255,255,0);">http://localhost:8000/event/{{$eventsetresult['name']}}
-                                            »</a>
-                                         @endforeach
-                                         @endif
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 style="margin-top:40px;margin-bottom:17px; color: #909090;">Suchergebnisse ({{$eventsetresults->count()}})</h4>
 
-                                </div>
-                            </div>
-                        </div>
+                        @if(!empty($eventsetresults))
+
+                            @foreach($eventsetresults as $eventsetresult)
+                                <a href="http://localhost:8000/event/{{$eventsetresult['name']}}">
+                                    <div style="background-color:#f1f1f1;padding-top:16px;padding-right:20px;padding-bottom:6px;padding-left:9px;margin-top:16px;margin-left:-6px;">
+                                        <h5 style="color:#4f86c3;">{{ $eventsetresult['name'] }}</h5>
+                                    <p style="color:#313437;">{{ $eventsetresult['shortDescription'] }}</p>
+                                    </div>
+                                </a>
+                            @endforeach
+                        @endif
                     </div>
                 </div>
-    <br><br><br><br><br><br>
+            </div>
+        </div>
+        <br><br><br><br><br><br>
     </div>
 @endsection
