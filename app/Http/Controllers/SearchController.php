b130401e1664781df8b024899e6441ca9345fe8a
pweiss58 <pweiss@hof-university.de>
2018-07-09 11:03:10 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,9 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Eventset;
-use App\Location;
-use App\Artist;
-use Illuminate\Support\Facades\DB;
+
 
 class SearchController extends Controller
 {
@@ -16,18 +14,11 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         // kein keyword/ keine results -> errormsg
-        $error = ['error' => 'No results. Please use other keywords.'];
-
-        // sollte ein keyword haben
-        //if($request->has('search')) {
+        $errors = ['noresults' => "No results. Please use other keywords.",];
 
 
             $eventsetresults = Eventset::search($request->input('q'))->get();
 
-            return view('search.results', compact('eventsetresults'));
-
-        //}
-
-        //return return view('search.results', compact('eventsetresults', $error));
+            return view('search.results', compact('eventsetresults'), array('errors' => $errors));
     }
 }
