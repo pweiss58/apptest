1af53d86912a779950b1af7cc9ac3628de5ff15a
pweiss58 <pweiss@hof-university.de>
2018-07-09 10:01:11 +0200

    Merge branch 'master' of https://github.com/pweiss58/apptest
    # Conflicts:
    #	app/Http/Controllers/EventsetController.php

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -14,7 +14,6 @@
                         <h4 style="margin-top:40px;margin-bottom:17px; color: #909090;">Suchergebnisse ({{$eventsetresults->count()}})</h4>
 
                         @if(!empty($eventsetresults))
-
                             @foreach($eventsetresults as $eventsetresult)
                                 <a href="http://localhost:8000/event/{{$eventsetresult['name']}}">
                                     <div style="background-color:#f1f1f1;padding-top:16px;padding-right:20px;padding-bottom:6px;padding-left:9px;margin-top:16px;margin-left:-6px;">
